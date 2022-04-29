8c853196360bcbe9adc6ae708d3b50c7948fe6bd
GabbaMendonca <gabba.kumbuca@gmail.com>
2022-04-28 23:41:20 -0300

    Refatorando mascara de abertura
    Refatorado o JS da mascara de abertura.
    Inserido campo para circuitos de avento de vulto.
    Inserido opc evento de vulto em reclamçoes.
    Inserido botao para enviar mascara para o editor de texto

--- assets/js/abertura.js
+++ assets/js/abertura.js
@@ -1,164 +1,159 @@
-class Abertura {
+function pegarDadosMascaraAbertura() {
+    let data = {}
 
-    constructor() {
-        this._data = this._getView()
-    }
+    // cabeçalho
+    data["oi"] = document.querySelector("#oi").value
+    data["nome"] = document.querySelector("#nome").value.toUpperCase()
 
-    _getView() {
-        let data = {}
+    //=== esquerda ===
+    // radio
+    data["abCritico"] = document.getElementsByName("abRadioCritico")
+    data["abProativo"] = document.getElementsByName("abRadioProativo")
+    data["abReincidente"] = document.getElementsByName("abRadioReincidente")
 
-        // cabeçalho
-        data["oi"] = document.querySelector("#oi").value
-        data["nome"] = document.querySelector("#nome").value.toUpperCase()
+    //=== centro ===
+    data["abPerimetro"] = document.getElementsByName("abPerimetro")
+    data["abCliente"] = document.querySelector("#abCliente").value.toUpperCase()
+    data["abCPD"] = document.querySelector("#abCPD").value
+    data["abAcesso"] = document.querySelector("#abAcesso").value
+    data["abChamadoInterno"] = document.querySelector("#abChamadoInterno").value
+    data["abEmail"] = document.querySelector("#abEmail").value
+    data["abDescricao"] = document.querySelector("#abDescricao").value.toUpperCase()
+    data["abEventoMassivo"] = document.querySelector("#abEventoMassivo").value.toUpperCase()
 
-        //=== esquerda ===
-        // radio
-        data["abCritico"] = document.getElementsByName("abRadioCritico")
-        data["abProativo"] = document.getElementsByName("abRadioProativo")
-        data["abReincidente"] = document.getElementsByName("abRadioReincidente")
+    //=== direita ===
 
-        //=== centro ===
-        data["abCliente"] = document.querySelector("#abCliente").value.toUpperCase()
-        data["abCPD"] = document.querySelector("#abCPD").value
-        data["abAcesso"] = document.querySelector("#abAcesso").value
-        data["abChamadoInterno"] = document.querySelector("#abChamadoInterno").value
-        data["abEmail"] = document.querySelector("#abEmail").value
-        data["abDescricao"] = document.querySelector("#abDescricao").value.toUpperCase()
+    // Select
+    let select = document.querySelector('#abFalha');
+    data["abFalha"] = select.options[select.selectedIndex].text;
 
-        //=== direita ===
+    //Radio
+    // data["abTs"] = document.getElementsByName("abTs")
 
-        // Select
-        let select = document.querySelector('#abFalha');
-        data["abFalha"] = select.options[select.selectedIndex].text;
+    data["abEnergia"] = document.getElementsByName("abEnergia")
+    data["abReset"] = document.getElementsByName("abReset")
+    data["abTestes"] = document.getElementsByName("abTestes")
 
-        //Radio
-        // data["abTs"] = document.getElementsByName("abTs")
+    data["ab103"] = document.getElementsByName("ab103")
+    data["ab104"] = document.getElementsByName("ab104")
+    data["ab109"] = document.getElementsByName("ab109")
 
-        data["abEletrica"] = document.getElementsByName("abEletrica")
-        data["abReset"] = document.getElementsByName("abReset")
-        data["abTestes"] = document.getElementsByName("abTestes")
-
-        data["ab103"] = document.getElementsByName("ab103")
-        data["ab104"] = document.getElementsByName("ab104")
-        data["ab109"] = document.getElementsByName("ab109")
-
-        data["abProativo"] = document.getElementsByName("abRadioProativo")
-        data["abReincidente"] = document.getElementsByName("abRadioReincidente")
-        data["abBackup"] = document.getElementsByName("abBackup")
-
-        data["abPerimetro"] = document.getElementsByName("abPerimetro")
-
-        // Log
-        data["log"] = textareaLog.value
-
-        return data
-    }
-
-    limpar() {
-
-        // esquerda
-        //centro
-        document.querySelector("#abCliente").value = ''
-        document.querySelector("#abCPD").value = ''
-        document.querySelector("#abAcesso").value = ''
-        document.querySelector("#abChamadoInterno").value = ''
-        document.querySelector("#abEmail").value = ''
-        document.querySelector("#abDescricao").value = ''
-            //direita
-
-        textareaLog.value = ''
-
-    }
-
-    gerarMascara() {
-        this._makeMascara(this._data)
-    }
-
-    _makeMascara(data) {
-
-        // radio com reset, eletrica e testes
-        let radio = {
-            '0': 'SIM',
-            '1': 'NAO',
-            '2': 'N/A',
-            'false': '',
-        }
-
-        let radio2 = {
-            '0': 'VTAL',
-            '1': 'OI',
-            'false': '',
-        }
-
-        let opcReset = radio[verificaRadio(data.abReset)]
-        let opcEletrica = radio[verificaRadio(data.abEletrica)]
-        let opcTestes = radio[verificaRadio(data.abTestes)]
-
-        let opc103 = radio[verificaRadio(data.ab103)]
-        let opc104 = radio[verificaRadio(data.ab104)]
-        let opc109 = radio[verificaRadio(data.ab109)]
-
-        let opcProativo = radio[verificaRadio(data["abProativo"])]
-        let opcReincidente = radio[verificaRadio(data["abReincidente"])]
-        let opcBackup = radio[verificaRadio(data["abBackup"])]
-
-        let opcPerimetro = radio2[verificaRadio(data.abPerimetro)]
-
-        // let opcCritico = radio1[verificaRadio(this._data["abCritico"])]
-        // let opcTS = radio2[verificaRadio(data.abTs)]
+    data["abProativo"] = document.getElementsByName("abRadioProativo")
+    data["abReincidente"] = document.getElementsByName("abRadioReincidente")
+    data["abBackup"] = document.getElementsByName("abBackup")
 
 
-        let mascara = `MASCARA DE ABERTURA\n`
 
-        mascara += `PREMIUM | PERIMETRO: ${opcPerimetro} - PROTOCOLO DO CLIENTE: `
+    // Log
+    data["log"] = textareaLog.value
 
-        // Verifica se cliente tem chamado interno
-        if (data.abChamadoInterno != '') {
-            mascara += `${data.abChamadoInterno}\n`
-        } else {
-            mascara += `N/A\n`
-        }
+    return data
+}
 
-        mascara += `CLIENTE: ${data.abCliente} - TEL: ${data.abCPD}\n`
+// ================================
 
-        // Verifica se cliente tem endereço de email
-        mascara += `EMAIL DO CLIENTE: `
-        if (data.abEmail != '') {
-            mascara += `${data.abEmail}\n`
-        } else {
-            mascara += `N/A\n`
-        }
+function abVerificaStausRadiosAbertura(data) {
 
-        mascara += `RECLAMANTE: CEC-SP OI${data.oi} - ${data.nome}\n`
+    let radio = { '0': 'SIM', '1': 'NAO', '2': 'N/A', 'false': '' }
+    let radio2 = { '0': 'VTAL', '1': 'OI', 'false': '' }
 
-        if (data.abAcesso == "") {
-            mascara += `ACESSO: 08:00 AS 18:00 HS - RECLAMACAO: ${data.abFalha.toUpperCase()}\n`
-        } else {
-            mascara += `ACESSO: ${data.abAcesso} - RECLAMACAO: ${data.abFalha.toUpperCase()}\n`
-        }
+    data.abPerimetro = radio2[verificaRadio(data.abPerimetro)]
 
-        mascara += `CHECKLIST: REALIZADO RESET?:${opcReset}/SEM ENERGIA?:${opcEletrica}/AUTORIZA PARAR TESTES?:${opcTestes}\n`
-        mascara += `CHECKLIST: SINAL 103?${opc103}/SINAL 104?${opc104}/SINAL 109?${opc109}\n`
+    data.abReset = radio[verificaRadio(data.abReset)]
+    data.abEnergia = radio[verificaRadio(data.abEnergia)]
+    data.abTestes = radio[verificaRadio(data.abTestes)]
 
-        mascara += `DESCRICAO: PROATIVO:${opcProativo} / REINCIDENTE:${opcReincidente} / POSSUI BACKUP ATIVO:${opcBackup}\n`
-        mascara += `ENCERRAMENTO/AGENDAMENTO : 0800 282 5231 1/5/5\n`
-        mascara += `${data.abDescricao}`
+    data.ab103 = radio[verificaRadio(data.ab103)]
+    data.ab104 = radio[verificaRadio(data.ab104)]
+    data.ab109 = radio[verificaRadio(data.ab109)]
 
-        if (data.log != '') {
-            mascara += `\n--- LOG ---\n`
-            mascara += `${ data.log }`
-        }
+    data.abProativo = radio[verificaRadio(data.abProativo)]
+    data.abReincidente = radio[verificaRadio(data.abReincidente)]
+    data.abBackup = radio[verificaRadio(data.abBackup)]
 
-        copyToClipboard(mascara)
-    }
+    return data
+}
+
+function abVerificaChamadoInterno(data) {
+    data.abChamadoInterno = data.abChamadoInterno == '' ? "N/A" : data.abChamadoInterno
+    return data
+}
+
+function abVerificaEmailCliente(data) {
+    data.abEmail = data.abEmail == '' ? "N/A" : data.abEmail
+    return data
+}
+
+function abVerificaHorarioAcesso(data) {
+    data.abAcesso = data.abAcesso == '' ? '08:00 AS 18:00 HS' : data.abAcesso.toUpperCase()
+    return data
+}
+
+function abVerificaEventoMassivo(data) {
+    data.abDescricao += data.abEventoMassivo == '' ? '' : `--- CIRCUTOS ASSOCIADOS ---\n${ data.abEventoMassivo }`
+    return data
+}
+
+function abVerificaLog(data) {
+    data.log = data.log == '' ? '' : `--- LOG ---\n${ data.log }`
+    return data
+}
+
+function abMascaraDeAbertura(data) {
+
+    let mascara = `MASCARA DE ABERTURA
+PREMIUM | PERIMETRO: ${data.abPerimetro} - PROTOCOLO DO CLIENTE: ${data.abChamadoInterno}
+CLIENTE: ${data.abCliente} - TEL: ${data.abCPD}
+EMAIL DO CLIENTE: ${data.abEmail}
+RECLAMANTE: CEC-SP OI ${data.oi} - ${data.nome}
+ACESSO: ${data.abAcesso} - RECLAMACAO: ${data.abFalha.toUpperCase()}
+CHECKLIST: REALIZADO RESET?:${data.abReset}/SEM ENERGIA?:${data.abEnergia}/AUTORIZA PARAR TESTES?:${data.abTestes}
+CHECKLIST: SINAL 103?${data.ab103}/SINAL 104?${data.ab104}/SINAL 109?${data.ab109}
+DESCRICAO: PROATIVO:${data.abProativo} / REINCIDENTE:${data.abReincidente} / POSSUI BACKUP ATIVO:${data.abBackup}
+ENCERRAMENTO/AGENDAMENTO : 0800 282 5231 1/5/5
+${data.abDescricao}`
+
+    return mascara
+}
+
+function abGerarMascaraAbertura(data) {
+
+    data = abVerificaStausRadiosAbertura(data)
+    data = abVerificaChamadoInterno(data)
+    data = abVerificaEmailCliente(data)
+    data = abVerificaHorarioAcesso(data)
+    data = abVerificaEventoMassivo(data)
+    data = abVerificaLog(data)
+
+    return abMascaraDeAbertura(data)
+}
+
+// ================================
+
+function abButtonGerarMascaraAbertura() {
+    data = pegarDadosMascaraAbertura()
+    mascara = abGerarMascaraAbertura(data)
+    copyToClipboard(mascara)
+}
+
+function abButtonEnviarParaEditor() {
+    data = pegarDadosMascaraAbertura()
+    mascara = abGerarMascaraAbertura(data)
+    editorTextArea.value = mascara
+    editor()
 }
 
 function abButtonLimpar() {
-    a = new Abertura()
-    a.limpar()
-}
+    // esquerda
+    //centro
+    document.querySelector("#abCliente").value = ''
+    document.querySelector("#abCPD").value = ''
+    document.querySelector("#abAcesso").value = ''
+    document.querySelector("#abChamadoInterno").value = ''
+    document.querySelector("#abEmail").value = ''
+    document.querySelector("#abDescricao").value = ''
+    document.querySelector("#abEventoMassivo").value = ''
+        //direita
 
-function abGerarMascara() {
-    a = new Abertura()
-    a.gerarMascara()
+    textareaLog.value = ''
 }
