6dba32be61ba77ab58b9d5e15e4b0f8c99c94a43
Gabriel <gabba.kumbuca@gmail.com>
2022-07-20 00:11:20 -0300

    Inserindo a documentação para list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,9 +1,37 @@
+/**
+ * List.js
+ * 
+ * Here we have the functions responsible for 
+ * managing the lists.
+ * 
+ * Just use the List function passing the list ID as a
+ * parameter and use addLine to pass the contents
+ * of the line.
+ * 
+ * --
+ * Aqui temos as funções responsáveis por
+ * gerenciar as listas.
+ * 
+ * Basta usar a função List passando o ID da
+ * lista como parâmetro e usar addLine para passar o
+ * conteúdo da linha.
+ */
+
+/**
+ * Managing all application lists
+ * @param {string} IdNameList ID of the list.
+ * @returns
+ */
 function List(IdNameList) {
 
     function _getList(IdNameList) {
         return document.getElementById(IdNameList)
     }
 
+    /**
+     * Add line to a list
+     * @param {string} nameLine Receive the name of a line
+     */
     function addLine(nameLine) {
         const list = _getList(IdNameList)
 
@@ -15,6 +43,10 @@ function List(IdNameList) {
         storage.updateLocalStorage()
     }
 
+    /**
+     * Remove line to a list
+     * @param {string} idNumber Receive the ID of a line
+     */
     function removeLine(idNumber) {
         const lineList = document.getElementById(`${IdNameList}.${idNumber}`)
         lineList.remove()
@@ -27,6 +59,12 @@ function List(IdNameList) {
     return { addLine, removeLine }
 }
 
+/**
+ * Create a line for the list 
+ * @param {string} nameList Receive  the name of a list
+ * @param {string} nameLine Receive  the name of a line
+ * @returns 
+ */
 function LineList(nameList, nameLine) {
 
     const idNumber = generateID()
@@ -69,6 +107,11 @@ function LineList(nameList, nameLine) {
     return { createLineList, id, idNumber }
 }
 
+/**
+ * Create a button for the line
+ * @param {string} name Receive the name of a button
+ * @returns 
+ */
 function ButtonList(name) {
     return {
         p: document.createElement("p"),
@@ -84,10 +127,18 @@ function ButtonList(name) {
     }
 }
 
+/**
+ * Generates random numbers of ID's
+ * @returns {number} Number ID
+ */
 function generateID() {
     return Math.round(Math.random() * 1000)
 }
 
+/**
+ * Remove a line a the list
+ * @param {object} event Receive an object from the DOM
+ */
 function removeLineList(event) {
     event.preventDefault()
     var idSplit = event.target.id.split(".")
